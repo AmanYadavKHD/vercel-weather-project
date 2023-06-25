99fbce98d0f2e089c3fd67b76d30411600d8ab23
Aman Yadav <134760871+AmanYadavKHD@users.noreply.github.com>
2023-06-25 10:55:19 +0530

    Update index.js

--- BackEnd/index.js
+++ BackEnd/index.js
@@ -7,7 +7,7 @@ const app = express();
 app.use(bodyParser.urlencoded({ extended: true }));
 
 app.get("/", function (req, res) {
-    res.sendFile(__dirname + "/index.html");
+    res.redirect( "https://vercel-weather-project-iiig.vercel.app/");
 });
 
 app.post("/", function (req, res) {
